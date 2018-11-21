088ecf91bf64e5cdb8f02893375c80791b2d9dd9
Ahmed Eltaher <ahmedmahmoud.eltaher@gmail.com>
2018-11-21 23:54:47 +0100

    Migrate retrofit2 and DataRepository to kotlin

---
+++ app/src/main/java/com/task/data/remote/ServiceGenerator.kt
@@ -0,0 +1,69 @@
+package com.task.data.remote
+
+import com.google.gson.Gson
+import com.task.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Created by AhmedEltaher on 5/12/2016
+ */
+
+@Singleton
+class ServiceGenerator @Inject
+constructor(private val gson: Gson) {
+
+    //Network constants
+    private val TIMEOUT_CONNECT = 30   //In seconds
+    private val TIMEOUT_READ = 30   //In seconds
+    private val CONTENT_TYPE = "Content-Type"
+    private val API_KEY = "apikey"
+    private val CONTENT_TYPE_VALUE = "application/json"
+    private val API_KEY_VALUE = "0bac85d8945140b3bc8dde8aff16e329"
+
+    private val okHttpBuilder: OkHttpClient.Builder = OkHttpClient.Builder()
+    private var retrofit: Retrofit? = null
+
+    private var headerInterceptor = Interceptor { chain ->
+        val original = chain.request()
+
+        val request = original.newBuilder()
+                .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
+                .header(API_KEY, API_KEY_VALUE)
+                .method(original.method(), original.body())
+                .build()
+
+        chain.proceed(request)
+    }
+
+    private val logger: HttpLoggingInterceptor
+        get() {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            if (BuildConfig.DEBUG) {
+                loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS).level = HttpLoggingInterceptor.Level.BODY
+            }
+            return loggingInterceptor
+        }
+
+    init {
+        okHttpBuilder.addInterceptor(headerInterceptor)
+        okHttpBuilder.connectTimeout(TIMEOUT_CONNECT.toLong(), TimeUnit.SECONDS)
+        okHttpBuilder.readTimeout(TIMEOUT_READ.toLong(), TimeUnit.SECONDS)
+    }
+
+    fun <S> createService(serviceClass: Class<S>, baseUrl: String): S {
+        val client = okHttpBuilder.build()
+        retrofit = Retrofit.Builder()
+                .baseUrl(baseUrl).client(client)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build()
+        return retrofit!!.create(serviceClass)
+    }
+}
