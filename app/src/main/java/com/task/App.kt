a35df51bb4479bf67fbd1369218c9eee93f901e7
Ahmed Eltaher <ahmed.mohamed@momox.biz>
2018-11-22 00:08:55 +0100

    Migrate App class to kotlin

---
+++ app/src/main/java/com/task/App.kt
@@ -0,0 +1,33 @@
+package com.task
+
+import android.content.Context
+import androidx.annotation.VisibleForTesting
+import androidx.multidex.MultiDexApplication
+
+import com.task.di.DaggerMainComponent
+import com.task.di.MainComponent
+
+/**
+ * Created by AhmedEltaher on 5/12/2016
+ */
+
+class App : MultiDexApplication() {
+    var mainComponent: MainComponent? = null
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+        mainComponent = DaggerMainComponent.create()
+        context = applicationContext
+    }
+
+    @VisibleForTesting
+    fun setComponent(mainComponent: MainComponent) {
+        this.mainComponent = mainComponent
+    }
+
+    companion object {
+        var context: Context? = null
+            private set
+    }
+}
