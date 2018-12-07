ea9c0a26662493a65b05fa5228a75ef64e29bc29
Ahmed Eltaher <ahmed.mohamed@momox.biz>
2018-12-07 14:21:45 +0100

    Migrate esspreso testing to kotlin

--- app/src/androidTest/java/com/task/ui/component/news/HomeActivityTest.java
+++ app/src/androidTest/java/com/task/ui/component/news/HomeActivityTest.java
@@ -1,6 +1,16 @@
 package com.task.ui.component.news;
 
 
+import com.task.App;
+import com.task.R;
+
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingResource;
 import androidx.test.espresso.ViewInteraction;
@@ -9,15 +19,6 @@ import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
-
-import com.task.R;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
@@ -28,11 +29,8 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static com.task.utils.ResourcesUtil.getString;
-import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.not;
 
-@LargeTest
 @RunWith(AndroidJUnit4.class)
 public class HomeActivityTest {
     private final String testSearchString = "the";
@@ -43,13 +41,13 @@ public class HomeActivityTest {
     private IdlingResource mIdlingResource;
 
     @Before
-    public void setup() {
+    public void setup () {
         mIdlingResource = mActivityTestRule.getActivity().getCountingIdlingResource();
         Espresso.registerIdlingResources(mIdlingResource);
     }
 
     @Test
-    public void testSearch() {
+    public void testSearch () {
         ViewInteraction searchEditText = onView(withId(R.id.et_search));
         searchEditText.perform(click());
         searchEditText.perform(ViewActions.typeText(testSearchString), pressImeActionButton());
@@ -63,15 +61,15 @@ public class HomeActivityTest {
     }
 
     @Test
-    public void testScroll() {
+    public void testScroll () {
         onView(ViewMatchers.withId(R.id.rv_news_list))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+            .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         onView(withId(R.id.tv_title)).check(matches(isDisplayed()));
         onView(withId(R.id.tv_description)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void testRefresh() {
+    public void testRefresh () {
         //Before refresh there is a list .
         onView(withId(R.id.rv_news_list)).check(matches(isDisplayed()));
         onView(withId(R.id.pb_loading)).check(matches((not(isDisplayed()))));
@@ -83,34 +81,32 @@ public class HomeActivityTest {
     }
 
     @Test
-    public void displayNewsData() {
+    public void displayNewsData () {
         onView(withId(R.id.rv_news_list)).check(matches(isDisplayed()));
         onView(withId(R.id.pb_loading)).check(matches((not(isDisplayed()))));
     }
 
     @Test
-    public void searchIsActive() {
+    public void searchIsActive () {
         onView(withId(R.id.rl_search)).check(matches(isDisplayed()));
         onView(withId(R.id.et_search)).check(matches(isDisplayed()));
         onView(withId(R.id.btn_search)).check(matches(isDisplayed()));
     }
 
     @After
-    public void unregisterIdlingResource() {
+    public void unregisterIdlingResource () {
         if (mIdlingResource != null) {
             Espresso.unregisterIdlingResources(mIdlingResource);
         }
     }
 
-    public void testEmptySearch() {
+    public void testEmptySearch () {
         String testSearchString = "";
         ViewInteraction searchEditText = onView(withId(R.id.et_search));
         searchEditText.perform(click());
         searchEditText.perform(ViewActions.typeText(testSearchString), pressImeActionButton());
         onView(withId(R.id.btn_search)).perform(click());
-        onView(allOf(withId(android.support.design.R.id.snackbar_text),
-                withText(INSTANCE.getString(R.string.search_error))))
-                .check(matches(isDisplayed()));
+        onView(Matchers.allOf(withId(R.id.snackbar_text), withText(App.context.getString(R.string.search_error)))).check(matches(isDisplayed()));
     }
 
 }
