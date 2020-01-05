fa0b6c2e7221642b1bfeb18301f1531ee88a10b7
ahmedeltaher <ahmedmahmoud.eltaher@gmail.com>
2020-01-05 14:16:29 +0100

    code improvments, Remove Data Status, Meta Data class , Optimize imports

--- app/src/main/java/com/task/data/remote/Data.kt
+++
@@ -1,9 +0,0 @@
-package com.task.data.remote
-
-import com.task.data.error.Error
-
-/**
- * Created by AhmedEltaher on 5/12/2016
- */
-
-class Data(var code: Int = 0, var error: Error? = null, var data: Any? = null)
